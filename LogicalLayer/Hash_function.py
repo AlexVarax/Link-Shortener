ad8406538f6e3a83181e97366ea8a8d8253565a0
Alex Varaxin <varaaleksei@yandex.ru>
2025-11-14 18:53:16 +0500

    Hash implementation and database communication 2

--- LogicalLayer/Hash_function.py
+++ LogicalLayer/Hash_function.py
@@ -1,11 +1,8 @@
-import xxhash
+from xxhash import xxh32 
 
 class Hash:
     def __init__(self):
         pass
 
     def hash(self, id, word: str):
-        return str(xxhash.xxh32(word, seed = id%2147483648).hexdigest())
-
-h = Hash
-print(h.hash(h, 3, "dsad"))
+        return str(xxh32(word, seed = id%2147483648).hexdigest())
---
+++ LogicalLayer/main.py
@@ -0,0 +1,7 @@
+import DataLayer.DB_Connect
+import NetworkLayer.Server
+import Hash_function
+
+
+
+
